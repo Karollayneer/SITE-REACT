a58e4b0ad0568ccc3aa1dc1001c30308ccb009f9
Pablo Cardoso <pablo.cardoso@hotmail.com>
2023-05-18 23:22:52 -0300

    Criação dos primeiros componentes e config de rotas

--- site-react/src/main.jsx
+++ site-react/src/main.jsx
@@ -3,8 +3,44 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// configurando router
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import Home from './routes/Home.jsx'
+import Contato from './routes/Contato.jsx'
+import ErrorPage from './routes/ErrorPage.jsx'
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Home />
+//   },
+//   {
+//     path: 'contato',
+//     element: <Contato />
+//   }
+// ]);
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+      path: '/',
+      element: <Home />
+      },
+      {
+      path: 'contato',
+      element: <Contato />
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
