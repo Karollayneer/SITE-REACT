fad9395a21752b91f2656fcb4501fe6eab31e71e
Karolayne Rodrigues <karollayneerod@gmail.com>
2023-05-27 21:54:29 -0300

    pagina de contato
    formulario de contato

--- src/pags/Contato.jsx
+++ src/pags/Contato.jsx
@@ -1,15 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Contato.css";
 
-
-
 const Contato = () => {
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [mensagem, setMensagem] = useState("");
+  const [enviado, setEnviado] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Código para processar os dados do formulário aqui
+    console.log("Dados do formulário:", { nome, email, mensagem });
+    setNome("");
+    setEmail("");
+    setMensagem("");
+    setEnviado(true);
+  };
+
   return (
-
-    <main>
-      <h1>Contatos</h1>
-    </main>
-
+    <div className="contato-container">
+      <h1>Contato</h1>
+      {!enviado ? (
+        <form onSubmit={handleSubmit}>
+          <div className="campo-formulario">
+            <label htmlFor="nome">Nome:</label>
+            <input
+              type="text"
+              id="nome"
+              value={nome}
+              onChange={(e) => setNome(e.target.value)}
+              required
+            />
+          </div>
+          <div className="campo-formulario">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="campo-formulario">
+            <label htmlFor="mensagem">Mensagem:</label>
+            <textarea
+              id="mensagem"
+              value={mensagem}
+              onChange={(e) => setMensagem(e.target.value)}
+              required
+            ></textarea>
+          </div>
+          <button type="submit">Enviar</button>
+        </form>
+      ) : (
+        <div className="enviado">
+          <p>Enviado com sucesso!</p>
+        </div>
+      )}
+    </div>
   );
 };
 
